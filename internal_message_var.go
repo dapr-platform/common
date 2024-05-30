2c56d2032d02e9a855958dee28c05e9f376e9e71
lyl <liuyulong@defensedriver.com.cn>
2024-05-30 11:44:41 +0800

    新文件：   internal_message_var.go

---
+++ internal_message_var.go
@@ -0,0 +1,46 @@
+// internal message 内部服务间各类消息
+package common
+
+import (
+	"encoding/json"
+)
+
+var INTERNAL_MESSAGE_TOPIC_NAME = "internal_msg"
+
+var INTERNAL_MESSAGE_TYPE_USER_LOGIN = "user_login"
+var INTERNAL_MESSAGE_TYPE_USER_LOGOUT = "user_logout"
+var INTERNAL_MESSAGE_TYPE_SYS_LOG = "sys_log"
+
+type InternalMessage map[string]any
+
+var INTERNAL_MESSAGE_KEY_TYPE = "type"
+var INTERNAL_MESSAGE_KEY_USER_ID = "user_id"
+
+func (m InternalMessage) GetType() string {
+	val := m[INTERNAL_MESSAGE_KEY_TYPE]
+	return val.(string)
+}
+
+func (m InternalMessage) FromStruct(s any) (err error) {
+	buf, err := json.Marshal(s)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(buf, &m)
+}
+
+type SyslogMessage struct {
+	Type   string `json:"type"`
+	UserId string `json:"user_id"`
+	Ip     string `json:"ip"`
+	Action int    `json:"action"`
+	Info   string `json:"info"`
+}
+
+func (s *SyslogMessage) FromInternalMessage(m InternalMessage) {
+	s.Type = m["type"].(string)
+	s.UserId = m["user_id"].(string)
+	s.Ip = m["ip"].(string)
+	s.Action = int(m["action"].(float64))
+	s.Info = m["info"].(string)
+}
