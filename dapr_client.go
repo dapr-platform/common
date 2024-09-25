6736903816e53e7fe23b6539c9d09421c746c047
lyl <liuyulong@defensedriver.com.cn>
2024-09-25 11:36:15 +0800

    update getDaprClient

--- dapr_client.go
+++ dapr_client.go
@@ -15,32 +15,38 @@ import (
 var daprClient client.Client
 
 const (
-	daprPortDefault    = "50001"
 	daprPortEnvVarName = "DAPR_GRPC_PORT"
+	daprHostEnvVarName = "DAPR_GRPC_HOST"
 	retryInterval      = 5 * time.Second // 定义重试间隔
 )
 
-var once = sync.Once{}
+var (
+	daprPort = "50001"
+	daprHost = "127.0.0.1"
+)
 
-func getDaprPort() string {
-	port := os.Getenv(daprPortEnvVarName)
-	if port == "" {
-		port = daprPortDefault
+func init() {
+	if p := os.Getenv(daprPortEnvVarName); p != "" {
+		daprPort = p
 	}
-	return port
+	if h := os.Getenv(daprHostEnvVarName); h != "" {
+		daprHost = h
+	}
 }
+
+var once = sync.Once{}
 
 func GetDaprClient() client.Client {
 	once.Do(func() {
-		port := getDaprPort()
+
 		for {
 			maxRequestBodySize := 16384
 			var opts []grpc.CallOption
 
 			headerBuffer := 1
 			opts = append(opts, grpc.MaxCallRecvMsgSize((maxRequestBodySize+headerBuffer)*1024*1024))
-			conn, err := grpc.Dial(net.JoinHostPort("127.0.0.1",
-				port),
+			conn, err := grpc.NewClient(net.JoinHostPort(daprHost,
+				daprPort),
 				grpc.WithDefaultCallOptions(opts...), grpc.WithTransportCredentials(insecure.NewCredentials()))
 			if err != nil {
 				fmt.Printf("Failed to create Dapr client: %v, retrying in %v...\n", err, retryInterval)
